636d0252ebdbe1dd7e790fa464246cdcf6a7a34c
Brayan Cavalcanti <bryan-de-sa@hotmail.com>
2022-06-19 12:57:38 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//Funções dos meus botoes HTML
+
+
+
+function AbreLinkedin(){
+
+  window.open('https://www.linkedin.com/in/brayan-cavalcanti-9b9979212/', '_blank');
+}
+
+function AbreCurriculo(){
+
+  window.open('https://www.canva.com/design/DAElagzOuG0/jLZtWZu49LigXhm6Zs0Chw/view?utm_content=DAElagzOuG0&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton', '_blank');
+}
+function AbreGithub(){
+
+window.open('https://github.com/Bry055');
+}
+
+function AbreReplit(){
+
+window.open('https://replit.com/@bry055/DEVSITE#index.html');
+}
